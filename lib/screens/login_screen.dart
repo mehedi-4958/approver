d8a3c1287f0a39dab038128ae7677e85258292f1
Mehedi <babu.oshi@gmail.com>
2021-07-06 10:57:37 +0600

    TextForms and Button Designed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,188 +17,207 @@ class _LoginPageState extends State<LoginPage> {
         body: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                height: 300,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.blue,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      height: 130.0,
-                      width: 130.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 123,
-                            width: 123,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.blue,
-                              image: DecorationImage(
-                                image: AssetImage('assets/images/splash.png'),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.blue,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 130.0,
+                        width: 130.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 123,
+                              width: 123,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue,
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/splash.png'),
+                                ),
                               ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 40,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(),
+                SizedBox(
+                  height: 150.0,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 300.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.blue.shade100,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(45),
+                            bottomLeft: Radius.circular(45),
+                          ),
+                        ),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Username',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
                             ),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'Sign in',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 40,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 150.0,
-              ),
-              Container(
-                height: 50.0,
-                width: 300.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.blue.shade100,
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45),
-                          bottomLeft: Radius.circular(45),
-                        ),
-                      ),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Username',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 2,
-                    ),
-                    Container(
-                      width: 194,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(45),
-                          bottomRight: Radius.circular(45),
-                        ),
+                      SizedBox(
+                        width: 2,
                       ),
-                      child: TextFormField(
-                        showCursor: false,
-                        keyboardType: TextInputType.text,
-                        style: TextStyle(
-                          color: Colors.blue,
+                      Container(
+                        width: 194,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(45),
+                            bottomRight: Radius.circular(45),
+                          ),
                         ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 50.0,
-                width: 300.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.blue.shade100,
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45),
-                          bottomLeft: Radius.circular(45),
-                        ),
-                      ),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Username',
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
                           style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                            color: Colors.blue,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 2,
-                    ),
-                    Container(
-                      width: 194,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(45),
-                          bottomRight: Radius.circular(45),
-                        ),
-                      ),
-                      child: TextFormField(
-                        autofocus: true,
-                        showCursor: false,
-                        keyboardType: TextInputType.text,
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 300.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.blue.shade100,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(45),
+                            bottomLeft: Radius.circular(45),
+                          ),
+                        ),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Password',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Container(
+                        width: 194,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(45),
+                            bottomRight: Radius.circular(45),
+                          ),
+                        ),
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 300.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('SIGNUP'),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
